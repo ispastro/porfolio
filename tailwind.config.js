e7617d79f1a1d6dab07df886a12707c4a2308bab
Haile Asaye <haileasaye51@gmail.com>
2025-01-29 21:58:06 +0300

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust this pattern to match your source files
+    "!./node_modules/**/*", // Exclude node_modules
+  ],
   darkMode: "class",
   theme: {
     extend: {},
